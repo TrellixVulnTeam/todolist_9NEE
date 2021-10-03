ba39c015372bd0d45a880064071d026e1ea53d87
proggerkid <proggerkid@yahoo.com>
2021-10-03 15:30:48 +0200

    old

--- app.js
+++ app.js
@@ -9,15 +9,16 @@ app.set('view engine', 'ejs')
 app.use(express.static('./public'))
 
 
-let uri = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.hprah.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+let uri = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.hprah.mongodb.net/sample_analytics?retryWrites=true&w=majority';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 async function run(){
 	try{
 		await client.connect()
-		await client.db('admin').command({ping:1})
 		console.log("connected to mongodb")
+		let results = await client.accounts.find({limits:10000})
+		console.log(results)
 	}
 	finally{
 		await client.close()
