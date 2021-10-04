aae2ba2d2cd407c5747bff1b0fdbc4c67f84587f
unavailablename <sieber.thomas@yahoo.com>
2021-10-04 18:27:48 +0200

    new

--- app.js
+++ app.js
@@ -23,12 +23,18 @@ let uri = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.hprah.mongodb.
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-///////////Render Manpage/////////////
+///////////Render Mainpage/////////////
 app.get('/', (req, res)=>{
 	if(req.session.user == undefined){
 		res.render('index', {todos: false, login: true})
 	}
 	else{
+		let username = req.session.user.name;
+
+		getAllTasks(username).then((allTasks)=>{
+			res.send(JSON.stringify(allTasks))
+		})
+		
 		res.render('index', {todos: true, login: false})
 	}
 })
@@ -40,9 +46,24 @@ app.post('/login', (req, res)=>{
 	loginUser(user, req, res).catch(console.dir)
 })
 
+//////////////Add New Task///////////////////////
 app.post('/addTask', upload.none(), (req, res)=>{
-	let data = req.body;
-	addTask(req, res)
+	let task = req.body.task;
+	let username = req.session.user.name;
+	
+	addTask(task, username)
+	getAllTasks(username).then((allTasks)=>{
+		res.send(JSON.stringify(allTasks))
+	})
+})
+
+//////Send Todos when index.ejs is send/////////
+app.get('/getTodos', (req, res)=>{
+	let name = req.session.user.name;
+	
+	getAllTasks(name).then((allTasks)=>{
+		res.send(JSON.stringify(allTasks))
+	})
 })
 
 
@@ -72,12 +93,51 @@ async function loginUser(user, req, res){
 	}
 }
 
-async function addTask(req, res){
+async function addTask(task, username){
 	try{
-		let newTask = req.body.task;
-		let user = req.session.user.username;
-
+		let newTask = task; 
+		let name = username;
+		
 		await client.connect()
 		
+		const db = client.db('todoapp');
+		const collection = db.collection('tasks');
+		const filter = {name: name};
+		const updateDoc = {
+			$push:{
+				tasks: newTask
+			}
+		};
+		
+		const result = await collection.updateOne(filter, updateDoc);
+	}
+	catch{
+	}
+	finally{
+		await client.close()
 	}
 }
+
+async function getAllTasks(username){
+	let name = username;
+	
+	try{
+		await client.connect()
+
+		const db = client.db('todoapp');
+		const tasks = db.collection('tasks');
+		const query = {name: name};
+		const options = {
+			projection: {tasks: 1, _id: 0}
+		}
+
+		const allTasks = await tasks.findOne(query, options);
+		return allTasks;
+	}
+	catch{
+	}
+	finally{
+		await client.close()
+	}
+}
+
--- public/main.js
+++ public/main.js
@@ -1,19 +1,44 @@
 let app = new Vue({
 	el: '#todolist',
 	data: {
-		todos: ['first', 'second', 'third'],
+		todos: [],
 		input_task: ""
+	},
+	computed: {
+		getTodos: function(){
+			let self = this;
+
+			if(this.todos.length == 0){
+				let xhr = new XMLHttpRequest();
+				xhr.open('get', '/getTodos', true)
+				xhr.send()
+				xhr.onload = function(){
+					let allTasks = JSON.parse(xhr.response).tasks;
+					for(task of allTasks){
+						self.todos.push(task)
+					}
+				}
+			}
+		}
 	},
 	methods:{
 		addTask: function(){
 			let xhr = new XMLHttpRequest()
 			let data = new FormData()
+			let self = this;
 			data.append('task', this.input_task)
 			xhr.open('post', '/addTask', true)
 			xhr.send(data)
 			xhr.onload = function(){
-				console.log(xhr.response)
+				let newTasks = JSON.parse(xhr.response).tasks;
+				self.todos = [];
+				for(task of newTasks){
+					self.todos.push(task)
+				}
 			}
+		},
+		deleteTask: function(){
+			
 		}
 	}
 });
